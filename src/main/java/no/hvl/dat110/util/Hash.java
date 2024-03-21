0896eec400643d599e0c03e8d07af4ba7e728e26
h602408 <602408@stud.hvl.no>
2024-03-21 14:28:31 +0100

    gjordt  hash test

--- src/main/java/no/hvl/dat110/util/Hash.java
+++ src/main/java/no/hvl/dat110/util/Hash.java
@@ -17,26 +17,41 @@ public class Hash {
 	public static BigInteger hashOf(String entity) {	
 		
 		BigInteger hashint = null;
-		
-		// Task: Hash a given string using MD5 and return the result as a BigInteger.
-		
-		// we use MD5 with 128 bits digest
-		
-		// compute the hash of the input 'entity'
-		
-		// convert the hash into hex format
-		
-		// convert the hex into BigInteger
-		
-		// return the BigInteger
-		
+try {
+
+
+	// Task: Hash a given string using MD5 and return the result as a BigInteger.
+	MessageDigest mdToBigInt = MessageDigest.getInstance("MD5");
+
+	// we use MD5 with 128 bits digest
+	mdToBigInt.update(entity.getBytes());
+
+	// compute the hash of the input 'entity'
+	byte[] input = mdToBigInt.digest();
+
+	// convert the hash into hex format
+	String hexString = toHex(input);
+
+	// convert the hex into BigInteger
+	 hashint = new BigInteger(hexString, 16);
+
+}
+catch (NoSuchAlgorithmException e) {
+	e.printStackTrace();
+
+	// return the BigInteger
+
+
+}
+
 		return hashint;
 	}
 	
 	public static BigInteger addressSize() {
 		
 		// Task: compute the address size of MD5
-		
+		int numberOfBits = bitSize();
+
 		// compute the number of bits = bitSize()
 		
 		// compute the address size = 2 ^ number of bits
